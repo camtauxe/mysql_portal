d97a007643cd8d79b5c10d7f0ddfa6336c2b5d8e
cameron@HITAGI <camerontauxe@gmail.com>
2018-10-03 17:35:56 -0600

    cleanup and documentation

--- phase2/deploy/lib/sqlutils.py
+++ phase2/deploy/lib/sqlutils.py
@@ -1,7 +1,15 @@
 import  mysql.connector
 
-from .config     import *
+from .config import *
 import cgiutils
+
+# Contains functions for interacting with
+# the MySQL Database
+# Maintains a current connection as a global variable.
+# The connection can be established by calling connect()
+# and closed by calling close().
+# Most functions will automatically establish the connection
+# first if they are called before the connection is established
 
 current_connection = None
 
@@ -28,6 +36,11 @@ def connect():
     except mysql.connector.Error as err:
         error_and_exit(err)
 
+# Get a list of the names of all of the tables in the database
+# If an error occurs, a 500 HTTP response will be printed
+# and the script will immediately exit
+#
+# If there is no current connection, it will be established
 def get_tables():
     global current_connection
     if current_connection is None:
@@ -36,16 +49,21 @@ def get_tables():
     try :
         cursor = current_connection.cursor()
         cursor.execute("SHOW TABLES")
-        return cursor.fetchall()
+        tuples = cursor.fetchall()
+        return list(map(lambda x: x[0], tuples))
     except mysql.connector.Error as err:
         error_and_exit(err)
 
+# Close the current connection
+# If there is no current connection, this does nothing
 def close():
     global current_connection
     if current_connection is not None:
         current_connection.close()
         current_connection = None
 
+# Print a 500 HTTP Response to stdout
+# and immediately exit
 def error_and_exit(err):
     cgiutils.print_error500(
             "An error occured connecting to the database!\n"+
--- phase2/deploy/tables.py
+++ phase2/deploy/tables.py
@@ -6,9 +6,16 @@ import cgitb
 from lib import cgiutils
 from lib import sqlutils
 
-cgitb.enable()
+# Respond with a list (in JSON format) of the names of all of
+# the tables in the database
 
-tables = list(map(lambda x: x[0], sqlutils.get_tables()))
+# Get the list of tables and convert to a JSON string
+tables = sqlutils.get_tables()
 json = json.dumps(tables)
 
+# Calling get_tables() established a database connection.
+# Since we're done with it, we close the connection
+sqlutils.close()
+
+# Print the HTTP response
 cgiutils.print_response(json, "200 OK", "text/json")
